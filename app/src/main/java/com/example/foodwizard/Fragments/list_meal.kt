8835758596b351a5dcb7b34c1fffa7bee711835d
Uradouby <1468247858@qq.com>
2023-04-11 20:53:30 -0400

    finish recycle view of meal

--- app/src/main/java/com/example/foodwizard/Fragments/list_meal.kt
+++ app/src/main/java/com/example/foodwizard/Fragments/list_meal.kt
@@ -1,14 +1,34 @@
 package com.example.foodwizard.Fragments
 
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodwizard.Adapter.mealAdapter
+import com.example.foodwizard.Meal
 import com.example.foodwizard.R
 import com.example.foodwizard.databinding.FragmentListMealBinding
 
+class MarginItemDecoration(private val spaceSize: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        with(outRect) {
+            if (parent.getChildAdapterPosition(view) == 0) {
+                top = spaceSize
+            }
+            left = spaceSize
+            right = spaceSize
+            bottom = spaceSize
+        }
+    }
+}
 class list_meal : Fragment() {
     private var _binding: FragmentListMealBinding? = null
     private val binding
@@ -27,6 +47,12 @@ class list_meal : Fragment() {
     ): View? {
         _binding = FragmentListMealBinding.inflate(inflater, container, false)
         binding.mealRecyclerView.layoutManager=LinearLayoutManager(context)
+        binding.mealRecyclerView.addItemDecoration(
+            MarginItemDecoration(16)
+        )
+        var meals= mutableListOf<Meal>(Meal("apple"),Meal("hamburger"))
+        val adapter = mealAdapter(meals)
+        binding.mealRecyclerView.adapter = adapter
         return binding.root
     }
 
