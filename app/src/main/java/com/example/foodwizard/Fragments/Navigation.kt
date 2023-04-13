c42f5f038aaf113d01f48c063f065de82ddf8db0
Uradouby <1468247858@qq.com>
2023-04-13 17:59:26 -0400

    add bottom bar

--- app/src/main/java/com/example/foodwizard/Fragments/Navigation.kt
+++ app/src/main/java/com/example/foodwizard/Fragments/Navigation.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import com.example.foodwizard.R
 import com.example.foodwizard.databinding.FragmentNavigationBinding
 
 
@@ -20,8 +23,17 @@ class Navigation : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
+    fun InativeAll()
+    {
+        binding.camera.setImageResource(R.drawable.inactive_camera);
+        binding.recipe.setImageResource(R.drawable.inactive_recipe);
+        binding.shop.setImageResource(R.drawable.inactive_shop);
+        binding.personalinfo.setImageResource(R.drawable.inactive_personalinfo);
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -29,6 +41,30 @@ class Navigation : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentNavigationBinding.inflate(inflater, container, false)
+        binding.camera.setOnClickListener{
+            val result= "camera"
+            InativeAll()
+            binding.camera.setImageResource(R.drawable.active_camera);
+            setFragmentResult("MainActivity", bundleOf("bundleKey" to result))
+        }
+        binding.recipe.setOnClickListener{
+            val result= "recipe"
+            InativeAll()
+            binding.recipe.setImageResource(R.drawable.active_recipe);
+            setFragmentResult("MainActivity", bundleOf("bundleKey" to result))
+        }
+        binding.shop.setOnClickListener{
+            val result= "shop"
+            InativeAll()
+            binding.shop.setImageResource(R.drawable.active_shop);
+            setFragmentResult("MainActivity", bundleOf("bundleKey" to result))
+        }
+        binding.personalinfo.setOnClickListener{
+            val result= "peronalinfo"
+            InativeAll()
+            binding.personalinfo.setImageResource(R.drawable.active_personalinfo);
+            setFragmentResult("MainActivity", bundleOf("bundleKey" to result))
+        }
         return binding.root
     }
 
--- app/src/main/java/com/example/foodwizard/Main.kt
+++ app/src/main/java/com/example/foodwizard/Main.kt
@@ -3,12 +3,27 @@ package com.example.foodwizard
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.foodwizard.databinding.ActivityMainBinding
 import com.example.foodwizard.databinding.ActivityRegisterBinding
 
 class Main : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        supportFragmentManager
+            .setFragmentResultListener("MainActivity", this) { requestKey, bundle ->
+                // We use a String here, but any type that can be put in a Bundle is supported
+                val result = bundle.getString("bundleKey")
+                if (result != null) {
+                    Log.d("MainTrans",result)
+                }
+                // Do something with the result
+            }
+        setContentView(binding.root)
     }
 }
