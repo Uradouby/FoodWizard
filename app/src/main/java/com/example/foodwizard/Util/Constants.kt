a6f309d90974eb28bc91686b8a0f165b81fd99ac
Uradouby <1468247858@qq.com>
2023-05-11 19:53:54 -0400

    clean openAI key

--- app/src/main/java/com/example/foodwizard/Util/Constants.kt
+++ app/src/main/java/com/example/foodwizard/Util/Constants.kt
@@ -4,8 +4,8 @@ object Constants {
     // diet recognition and analysis api
     const val DIET_URL = "https://api.spoonacular.com/"
     // please enter the api key(from https://spoonacular.com/food-api)
-    const val DIET_REC_API_KEY = "55b00705aea048ef8aab5a798c015e97"
+    const val DIET_REC_API_KEY = ""
     const val TEMP_IMAGE_URL = "https://spoonacular.com/recipeImages/635350-240x150.jpg"
-    const val CHATGPT_KEY = "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"
+    const val CHATGPT_KEY = ""
     const val CHATGPT_URL  = "https://api.openai.com/v1/chat/completions"
 }
